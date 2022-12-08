5a43973e50955a20ec66c2423f3c7a2f4df9ac6c
Faten_khoder <Khoderfaten5@gmail.com>
2022-12-08 08:30:44 +0000

    01 Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+var a=document.getElementById('first_number');
+var b=document.getElementById('second_number');
+var calc=document.getElementById('validate');
+calc.addEventListener('click', function(){
+    alert("The result is: "+ a.value*b.value)
+});
